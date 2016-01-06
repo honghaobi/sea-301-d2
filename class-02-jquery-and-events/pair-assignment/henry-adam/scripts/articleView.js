9c6b345995e73c97c1a48cf25091aa1bd5be2ff2
henrybi <henrybi@u.washington.edu>
2016-01-05 16:26:55 -0800

    arcticleView js done with 4 todos.

--- class-02-jquery-and-events/pair-assignment/henry-adam/scripts/articleView.js
+++ class-02-jquery-and-events/pair-assignment/henry-adam/scripts/articleView.js
@@ -32,10 +32,15 @@ articleView.handleAuthorFilter = function() {
       // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
       //       and then show just the ones that match for the author that was selected.
       //       Use an "attribute selector" to find those articles, and fade them in for the reader.
+      $('article').hide();
+      var authorDisplay = $(this).val();
+      $('article').filter("[data-author = '" + authorDisplay + "']").fadeIn();
 
     } else {
       // TODO: If the select box was changed to an option that is blank, we should
       //       show all the articles, except the one article we are using as a template.
+      $('article').hide();
+
 
     }
     $('#category-filter').val('');
@@ -47,6 +52,27 @@ articleView.handleCategoryFilter = function() {
   //       When an option with a value is selected, hide all the articles, then reveal the matches.
   //       When the blank (default) option is selected, show all the articles, except for the template.
   //       Be sure to reset the #author-filter while you are at it!
+  $('#category-filter').on('change', function() {
+    if ($(this).val()) {
+      // TODO: If the select box was changed to an option that has a value, we need to hide all the articles,
+      //       and then show just the ones that match for the author that was selected.
+      //       Use an "attribute selector" to find those articles, and fade them in for the reader.
+      $('article').hide();
+      var categoryDisplay = $(this).val();
+      $('article').filter("[data-category = '" + categoryDisplay + "']").fadeIn();
+      $('#author-filter').reset();
+    } else {
+      // TODO: If the select box was changed to an option that is blank, we should
+      //       show all the articles, except the one article we are using as a template.
+      $('article').hide();
+
+    }
+    $('#category-filter').val('');
+  });
+
+
+
+
 
 };
 
@@ -74,3 +100,9 @@ articleView.setTeasers = function() {
 
 // TODO: Call all of the above functions, once we are sure the DOM is ready.
 $();
+
+$(document).ready(function(){
+articleView.populateFilters();
+articleView.handleAuthorFilter();
+articleView.handleCategoryFilter();
+});
